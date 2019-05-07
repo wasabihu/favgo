e39f0a446001d430e4bfc35cc81c66193b190e7b
Wasabi <wasabi.net@gmail.com>
2019-05-07 15:32:56 +0800

    fx

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ func init() {
 
 	// 参数4(可选)  设置最大空闲连接
 	// 参数5(可选)  设置最大数据库连接 (go >= 1.2)
-	maxIdle := 15
-	maxConn := 100
+	maxIdle := 10
+	maxConn := 30
 	orm.RegisterDataBase("default", "mysql", "root:123456@/www.wasa.ren?charset=utf8", maxIdle, maxConn)
 }
 
